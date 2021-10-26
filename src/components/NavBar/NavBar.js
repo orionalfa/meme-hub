45ec975f85694271393f5d1f963af8c7f4febc6e
orionalfa <orionalfacentauri@gmail.com>
2021-10-26 17:05:08 +0200

    navbar fixes

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -13,19 +13,17 @@ export default function NavBar() {
   return (
     <>
       <nav>
-        <Container>
-          <Row>
-            <Col sm={9}>
-              <Link to="/">Home</Link>
-            </Col>
-            <Col sm={2}>
-              <Link to="/register">Register</Link>
-            </Col>
-            <Col sm={1}>
-              <Link to="/login">Login</Link>
-            </Col>
-          </Row>
-        </Container>
+        <Row>
+          <Col sm={9}>
+            <Link to="/">Home</Link>
+          </Col>
+          <Col sm={2}>
+            <Link to="/register">Register</Link>
+          </Col>
+          <Col sm={1}>
+            <Link to="/login">Login</Link>
+          </Col>
+        </Row>
       </nav>
     </>
   );
